94338e21c723f325a4db75df9d264e8728893c3e
Guilherme Matos <marques.guilherme0067@gmail.com>
2020-12-05 00:53:26 -0300

    Get do endpoint products

--- front-end/src/pages/ShowEvents/index.js
+++ front-end/src/pages/ShowEvents/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { ContainerEvents } from './styles';
 
+import api from '../../services/api';
+
 function ShowEvents(){
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        api.get('/products').then((res) => {
+
+            const docs = res.data
+
+            setProducts(docs);
+            console.log(docs);
+        });
+
+    }, []);
+
     return(
         <ContainerEvents>
             Eventos
