a0c9902412500a4e90782c809634f61e1beb1b49
Guilherme Matos <marques.guilherme0067@gmail.com>
2020-12-07 22:16:39 -0300

    Funcao de deletar e atualizar

--- front-end/src/pages/EventsDetails/index.js
+++ front-end/src/pages/EventsDetails/index.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useHistory } from 'react-router-dom';
 
 import { ContainerEvents } from './styles';
+import UpdateEvent from '../../components/UpdadteEvent';
 import api from '../../services/api';
 
 import { AiOutlineArrowLeft } from 'react-icons/ai';
@@ -9,7 +10,10 @@ import { MdEdit, MdDelete } from 'react-icons/md';
 
 function EventsDetails(){
     const [product, setProduct] = useState([]);
+    const [show, setShow] = useState(false);
+    const [event, setEvent] = useState(true);
 
+    const history = useHistory();
     const { id } = useParams();
     
     useEffect(() => {
@@ -19,37 +23,86 @@ function EventsDetails(){
             const docs = res.data;
 
             setProduct(docs);
-            console.log(docs);
         });
 
     }, []);
 
+    function HandleDelete(){
+        api.delete(`/products/${id}`, {
+            id : id
+        }).then(() => {
+            alert('Evento deletado!');
+
+            history.push('/show-events');
+        }).catch(() => {
+            alert('Erro ao deletar!');
+        });
+    }
+
+    function HandleShow(){
+        setShow(true);
+        setEvent(false);
+    }
+
+    function HandleBack(){
+        setShow(false);
+        setEvent(true);
+    }
+
     return(
         <ContainerEvents>
-            <Link to='/show-events'> <AiOutlineArrowLeft className='icon' /> Voltar </Link>
 
-            <div className="InfoEvents">
-                <h1> {product.titulo} </h1>
-                <h2><strong>Descrição:</strong> {product.descricao} </h2>
+            {
+                show && 
 
-                <div className="contato">
-                    <p><strong>Email:</strong> {product.email}</p>
-                    <p><strong>Telefone:</strong> {product.telefone}</p>
+                <UpdateEvent 
+                    titulo = { product.titulo }
+                    descricao = { product.descricao }
+                    email = { product.email }
+                    telefone = { product.telefone }
+                    data = { product.data }
+                    hora = { product.hora }
+                    local = { product.local }
+                    endereco = { product.endereco }
+
+                    BtnVoltar = { HandleBack }
+                    id = { id }
+                />
+            }
+
+            {
+                event && 
+
+                <div className="Event">
+                    <Link to='/show-events'> <AiOutlineArrowLeft className='icon' /> Voltar </Link>
+                    
+                    <div className="InfoEvents">
+                        <h1> {product.titulo} </h1>
+                        <h2><strong>Descrição:</strong> {product.descricao} </h2>
+
+                        <div className="contato">
+                            <p><strong>Email:</strong> {product.email}</p>
+                            <p><strong>Telefone:</strong> {product.telefone}</p>
+                        </div>
+
+                        <div className="data-hora">
+                            <p><strong>Data:</strong> {product.data}</p>
+                            <p><strong>Hora:</strong> {product.hora}</p>
+                        </div>
+
+                        <p><strong>Local:</strong> {product.local}</p>
+                        <p><strong>Endreco:</strong> {product.endereco}</p>
+                    
+                        <div className="acitions">
+                            <i className='edit' onClick={ HandleShow }> <MdEdit /> </i>
+                            <i className='delete' onClick={ HandleDelete }> <MdDelete /> </i>
+                        </div>
+                    </div>
                 </div>
+            }
 
-                <div className="data-hora">
-                    <p><strong>Data:</strong> {product.data}</p>
-                    <p><strong>Hora:</strong> {product.hora}</p>
-                </div>
 
-                <p><strong>Local:</strong> {product.local}</p>
-                <p><strong>Endreco:</strong> {product.endereco}</p>
             
-                <div className="acitions">
-                    <i className='edit'> <MdEdit /> </i>
-                    <i className='delete'> <MdDelete /> </i>
-                </div>
-            </div>
         </ContainerEvents>
     );
 }
